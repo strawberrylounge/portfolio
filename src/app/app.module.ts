f809c027cf3039d590d9c4ac6a7616ff9afa3d07
strawberrylounge <raspberrylounge@gmail.com>
2020-12-26 18:58:27 +0900

    make a structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectsComponent } from './contents/projects/projects.component';
+import { BlogComponent } from './contents/blog/blog.component';
+import { ContactMeComponent } from './contents/contact-me/contact-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    BlogComponent,
+    ContactMeComponent
   ],
   imports: [
     BrowserModule,
