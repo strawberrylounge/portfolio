7af99cc0d1ec70fa1ecb59140a0a916d38b580e3
Cynthia Kim <raspberrylounge@gmail.com>
2025-05-27 15:25:01 +0900

    modified: 불필요한 구조 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header/Header.jsx";
-import Footer from "./components/Footer/Footer.jsx";
+import BtnTop from "./components/Buttons/BtnTop.jsx";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About/About.jsx";
 import Work from "./pages/Works/Works.jsx";
-import Contact from "./pages/Contact/Contact.jsx";
-import BtnTop from "./components/Buttons/BtnTop.jsx";
 
 function App() {
   return (
@@ -17,11 +15,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/Work" element={<Work />} />
-          {/* <Route path="/contact" element={<Contact />} /> */}
         </Routes>
       </main>
       <BtnTop />
-      {/* <Footer /> */}
     </Router>
   );
 }
